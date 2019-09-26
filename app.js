a8d04a899e21ef2eee893847a3d8bfc24f7cc735
Yael Brown <yaelrbrown@gmail.com>
2019-09-25 23:12:15 -0500

    Changed port for heroku to process_env/3000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
