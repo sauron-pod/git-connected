c5203d4fa3db7c201058267dce9c5dc72b503679
Yael Brown <yaelrbrown@gmail.com>
2019-09-25 23:05:57 -0500

    Created copy of server-json and named it app.js for heroku.

---
+++ app.js
@@ -0,0 +1,20 @@
+// This version of server-json is used for heroku deploys.
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+
+server.use('/api', router);
+
+
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
